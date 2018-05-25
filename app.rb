675147694a1addf63708cdbc2d3766800dec17ec
bembem123 <denver.aleta91@gmail.com>
2018-05-25 20:41:27 +0800

    add rb and yaml

---
+++ app.rb
@@ -0,0 +1,16 @@
+class CatTime < Sinatra::Base
+
+  set :port, 8080
+
+  get '/' do
+    @cat_pic = get_a_cat()
+    erb :index
+  end
+
+  def get_a_cat
+    client = CatAPI.new
+    cat_pic = client.get_images(results_per_page: 1).first
+
+    return cat_pic.url
+  end
+end
